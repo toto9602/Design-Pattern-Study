b53919b25d06662bf8a10342ff1a6dca50c7ef62
toto9602 <toto9602@naver.com>
2023-09-17 22:53:34 +0900

    factory pattern - 팩토리 메서드

---
+++ factory-pattern/california-pizza-store.java
@@ -0,0 +1,15 @@
+public class CaliforniaPizzaStore extends PizzaStore {
+    public Pizza createPizza(String item) {
+        if (item.equals("cheese")) {
+            return new CaliforniaStyleCheesePizza();
+        } else if (item.equals("veggie")) {
+            return new CaliforniaStyleVeggiePizza();
+        } else if (item.equals("potato")) {
+            return new CaliforniaStylePotatoPizza();
+        } else if (item.equals("pepperoni")) {
+            return new CaliforniaStylePepperoniPizza();
+        } else {
+            return null;
+        }
+    }
+}
